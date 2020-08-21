32f82e6c56d7da0add36c577a7ad09957da20e0d
jd <jean.daniel.michaud@gmail.com>
2020-08-21 17:25:04 +0200

    add a2dp bluetooth connection script

---
+++ on-connect-bluetooth.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This script checks if a a2dp bluetooth device is connected and if so, create the file /tmp/bluetooth-a2dp-connected.
+# Otherwise, it removes the file. To be used with i3status.
+
+# For the script to run on connect/disconnect of bluetooth devices, enable the following udev rules:
+# cat > /etc/udev/rules.d/42-bluetooth-i3status.rules
+# ACTION=="add", SUBSYSTEM=="bluetooth", RUN+="/bin/sh -c '/home/jedi/sh/on-connect-bluetooth.sh > /tmp/on-connect-bluetooth.log 2>&1'"
+# ACTION=="remove", SUBSYSTEM=="bluetooth", RUN+="/bin/sh -c '/home/jedi/sh/on-connect-bluetooth.sh > /tmp/on-connect-bluetooth.log 2>&1'"
+
+echo "Executing bluetooth script...|$ACTION|"
+
+ACTION=$(expr "$ACTION" : "\([a-zA-Z]\+\).*")
+if [ "$ACTION" = "add" -o "$ACTION" = "remove" ]
+then
+  sleep 1
+  CONFIRM=`bluetoothctl info | grep 0000110d-0000-1000-8000-00805f9b34fb`
+  if [ ! -z "$CONFIRM" ]
+  then
+    touch /tmp/bluetooth-a2dp-connected
+  else
+    rm -f /tmp/bluetooth-a2dp-connected        
+  fi
+fi
+
